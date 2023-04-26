c766b350fec68c57acc3b5145032bb7b938d15d4
Fahad Zahid <fahadzahidd24@gmail.com>
2023-04-26 09:25:49 -0400

    pug

--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,7 @@ router.get('/add-product',(req,res,next)=>{
 router.post('/add-product', (req,res,next)=>{
     products.push({title:req.body.title})
     res.redirect('/');
+    
 })
 
 exports.routes = router;
