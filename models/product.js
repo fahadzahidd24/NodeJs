fc1a14c47ae1788a507c8d8393f3769eb146181e
Fahad Zahid <fahadzahidd24@gmail.com>
2023-04-27 12:01:40 -0400

    sequelize

--- models/product.js
+++ models/product.js
@@ -1,34 +1,29 @@
-const fs = require('fs');
-const path = require('path');
-const dirPath = require('../util/path');
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
 
-const p = path.join(dirPath,'data','products.json');
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
-module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+const Product = sequelize.define('product',{
+  id:{
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price:{
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  description:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  imageUrl:{
+    type: Sequelize.STRING,
+    allowNull:false
   }
+})
 
-  save() {
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
-  }
-
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
-};
+module.exports = Product;
